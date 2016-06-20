c0ca3b67e05dc71bb6611150df89cf3e14cfe890
Corey Murphy <c.murphy360@gmail.com>
2016-06-19 19:07:13 -0700

    Connected Buttons from Main Activity to xml.
    Set OnClickListeners().
    
    Test SAT.

--- app/src/main/java/com/andrios/prt/MainActivity.java
+++ app/src/main/java/com/andrios/prt/MainActivity.java
@@ -1,5 +1,8 @@
 package com.andrios.prt;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,15 +11,30 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button profileBTN, calcBTN, aboutBTN, instructionBTN;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-       // setSupportActionBar(toolbar);
+        // setSupportActionBar(toolbar);
 
        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +44,76 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        setConnections();
+        setOnClickListeners();
+    }
+
+
+    private void setConnections() {
+        profileBTN = (Button) findViewById(R.id.mainActivityProfileBTN);
+        calcBTN = (Button) findViewById(R.id.mainActivityCalcBTN);
+        aboutBTN = (Button) findViewById(R.id.mainActivityAboutBTN);
+        instructionBTN = (Button) findViewById(R.id.mainActivityInstBTN);
+    }
+
+    private void setOnClickListeners() {
+        profileBTN.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Profile Button Clicked!", Toast.LENGTH_SHORT).show();
+
+            }
+
+
+           /* public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),
+                        ProfileActivity.class);
+                intent.putExtra("profile", profile);
+                startActivityForResult(intent, PROFILEVIEW);
+            }*/
+
+        });
+
+        calcBTN.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Calculator Button Clicked!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+            }
+
+        });
+        aboutBTN.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "About Button Clicked!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+            }
+
+        });
+        instructionBTN.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Instruction Button Clicked!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+            }
+
+        });
+
     }
 
     @Override
@@ -48,5 +136,45 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.andrios.prt/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.andrios.prt/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
